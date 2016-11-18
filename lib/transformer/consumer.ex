998b4f96c0c190a1c3aec67de9e7dd9ab70b4902
Alishan Ladhani <alishan@retailcommon.com>
2016-11-18 16:50:57 -0500

    Fix counter

--- lib/transformer/consumer.ex
+++ lib/transformer/consumer.ex
@@ -4,7 +4,7 @@ defmodule Transformer.Consumer do
   use GenStage
 
   def start_link do
-    GenStage.start_link(Consumer, %{file: File.open!("output.txt", [:write, :utf8]), counter: 0})
+    GenStage.start_link(Consumer, %{file: File.open!("output.txt", [:write, :utf8]), counter: 1})
   end
 
   def init(state) do
@@ -12,14 +12,16 @@ defmodule Transformer.Consumer do
   end
 
   def handle_events(events, _from, %{file: file, counter: counter}) do
-    Enum.each(events, & handle_event(&1, file, counter))
+    num_events = Enum.count(events)
 
-    # We are a consumer, so we would never emit items.
-    {:noreply, [], %{file: file, counter: counter + Enum.count(events)}}
+    IO.puts "Writing lines #{counter} to #{counter + num_events}..."
+
+    Enum.each(events, & handle_event(&1, file))
+
+    {:noreply, [], %{file: file, counter: counter + num_events + 1}}
   end
 
-  def handle_event(event, file, counter) do
-    IO.puts "Writing line #{counter}..."
+  def handle_event(event, file) do
     line = Enum.join(event, "|")
     IO.write(file, line)
   end
