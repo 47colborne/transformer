8936a21397274dc0a1778e4a84eb76504c630e3e
Alishan Ladhani <alishan@retailcommon.com>
2016-11-18 16:43:37 -0500

    Add counter for line numbers

--- lib/transformer/consumer.ex
+++ lib/transformer/consumer.ex
@@ -4,24 +4,22 @@ defmodule Transformer.Consumer do
   use GenStage
 
   def start_link do
-    GenStage.start_link(Consumer, File.open!("output.txt", [:write, :utf8]))
+    GenStage.start_link(Consumer, %{file: File.open!("output.txt", [:write, :utf8]), counter: 0})
   end
 
-  def init(file) do
-    {:consumer, file}
+  def init(state) do
+    {:consumer, state}
   end
 
-  def handle_events(events, _from, file) do
-    # Inspect the events.
-    IO.inspect(events)
-
-    Enum.each(events, & handle_event(&1, file))
+  def handle_events(events, _from, %{file: file, counter: counter}) do
+    Enum.each(events, & handle_event(&1, file, counter))
 
     # We are a consumer, so we would never emit items.
-    {:noreply, [], file}
+    {:noreply, [], %{file: file, counter: counter + Enum.count(events)}}
   end
 
-  def handle_event(event, file) do
+  def handle_event(event, file, counter) do
+    IO.puts "Writing line #{counter}..."
     line = Enum.join(event, "|")
     IO.write(file, line)
   end
