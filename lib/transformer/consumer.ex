7f55ea73e4a2b4059327568c85d378afb0a9f70d
Alishan Ladhani <alishan@retailcommon.com>
2016-11-18 16:38:39 -0500

    Write output to a file

--- lib/transformer/consumer.ex
+++ lib/transformer/consumer.ex
@@ -4,18 +4,25 @@ defmodule Transformer.Consumer do
   use GenStage
 
   def start_link do
-    GenStage.start_link(Consumer, :ok)
+    GenStage.start_link(Consumer, File.open!("output.txt", [:write, :utf8]))
   end
 
-  def init(:ok) do
-    {:consumer, :the_state_does_not_matter}
+  def init(file) do
+    {:consumer, file}
   end
 
-  def handle_events(events, _from, state) do
+  def handle_events(events, _from, file) do
     # Inspect the events.
     IO.inspect(events)
 
+    Enum.each(events, & handle_event(&1, file))
+
     # We are a consumer, so we would never emit items.
-    {:noreply, [], state}
+    {:noreply, [], file}
+  end
+
+  def handle_event(event, file) do
+    line = Enum.join(event, "|")
+    IO.write(file, line)
   end
 end
